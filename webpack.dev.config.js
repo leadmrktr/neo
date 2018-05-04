d0b9ab91b053ccb79bdabcdcf3ee51489319646b
Syam Pillai <syam.sp0965@gmail.com>
2018-05-05 00:44:51 +0530

    updated firebase

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -31,7 +31,7 @@ module.exports = {
         loader: 'babel-loader',
         query:{
           presets: ['es2015','react','stage-0']
-        }
+        },
         exclude: /node_modules/,
       },
       {
